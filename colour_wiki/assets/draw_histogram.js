af0ea90a5f8ef316922036d7d1055f64a28b8306
Inna <artistastudioxyz@gmail.com>
2020-04-30 14:35:22 +1000

    Added color descriptions (composition)
    1. added collapsible color descriptions
    2. added animated images for the dict start page

--- colour_wiki/assets/draw_histogram.js
+++ colour_wiki/assets/draw_histogram.js
@@ -6,30 +6,31 @@
     // default size: width 300px and height 150px
     var ctx = ctx.getContext('2d')
 
-    var tunit = width / 10;
+    var tunit = width / 12;
     var barwidth = tunit;
-    var barspace = 2 * tunit;
+    var barspace = 1.5 * tunit;
 
     if (histtype == 'rgb'){
         var datamax = 100;
         var label = ['R', 'G', 'B'];
         var indcolor = ['#FF0000', '#00FF00', '#152EFF'];
-        barcolor = ['#c9c9c9', '#c9c9c9', '#c9c9c9'];
+        //barcolor = ['#ff8080', '#80ff80', '#808dff'];
+        barcolor = indcolor;
     }
 
     if (histtype == 'cmyk'){
         var datamax = 100;
         var label = ['C', 'M', 'Y', 'K'];
-        var indcolor = ['#00FFFF', '#FF00FF', '#FFFF00', '#000000'];
-        barcolor = ['#c9c9c9', '#c9c9c9', '#c9c9c9', '#c9c9c9'];
+        var indcolor = ['#0BF9EA', '#FF00FF', '#FFFF00', '#000000'];
+        //barcolor = ['#7dfff7', '#ff80ff', '#ffff80', '#828282'];
+        barcolor = indcolor;
     }
 
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = '#e3e3e3';
-    ctx.lineCap = 'butt';
+    ctx.lineWidth = 6;
+    ctx.strokeStyle = '#e6e6e6';
     ctx.beginPath();
-    ctx.moveTo(0, 0.9 * width);
-    ctx.lineTo(width, 0.9 * width);
+    ctx.moveTo(0, 0.9 * height);
+    ctx.lineTo(width, 0.9 * height);
     ctx.stroke();
 
     var posX = 0;
@@ -40,29 +41,29 @@
         posX = i * (barspace + barwidth) + (width - data.length * barwidth - (data.length - 1) * barspace) / 2;
         posY =  height - barheight - 0.1 * height;
 
-        ctx.fillStyle = '#f2f2f2';
+        ctx.fillStyle = '#e6e6e6';
         ctx.fillRect(posX,  (0.1 * height), barwidth, (0.8 * height));
         ctx.fillStyle = barcolor[i];
         ctx.fillRect(posX, posY, barwidth, barheight);
 
         ctx.fillStyle = '#023148';
         ctx.font = '18pt Montserrat';
-        ctx.fillText(data[i], posX + 5, posY - 0.05 * height);
+        ctx.fillText(data[i], posX + 10, posY - 0.05 * height);
         ctx.font = 'bold 22pt Montserrat';
         ctx.fillText(label[i], posX + 10,  0.99 * height)
 
-        ctx.lineWidth = 8;
-        ctx.strokeStyle = indcolor[i];
-        ctx.beginPath();
-        ctx.moveTo(posX, 0.9 * height - barheight);
-        ctx.lineTo(posX + barwidth, 0.9 * height - barheight);
-        ctx.stroke();
+        //ctx.lineWidth = 2;
+        //ctx.strokeStyle = indcolor[i];
+        //ctx.beginPath();
+        //ctx.moveTo(posX + 0.5 * barwidth, 0.9 * height);
+        //ctx.lineTo(posX + 0.5 * barwidth, 0.9 * height - barheight);
+        //ctx.stroke();
 
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = indcolor[i];
-        ctx.beginPath();
-        ctx.moveTo(posX + 0.5 * barwidth, 0.9 * height);
-        ctx.lineTo(posX + 0.5 * barwidth, 0.9 * height - barheight);
-        ctx.stroke();
+        //ctx.lineWidth = 8;
+        //ctx.strokeStyle = indcolor[i];
+        //ctx.beginPath();
+        //ctx.moveTo(posX, 0.9 * height - barheight);
+        //ctx.lineTo(posX + barwidth, 0.9 * height - barheight);
+        //ctx.stroke();
     }
 }
