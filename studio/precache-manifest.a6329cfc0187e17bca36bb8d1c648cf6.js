3c54c489e1d8729ba11b05d8afd699fbdb8c267a
Ujjwal Kashyap <37990652+ujjwal-kashyap@users.noreply.github.com>
2020-05-13 09:24:52 +1000

    refreshing artista

---
+++ studio/precache-manifest.a6329cfc0187e17bca36bb8d1c648cf6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08637fb01db169c3c3e23a00ab595aeb",
+    "url": "/studio/index.html"
+  },
+  {
+    "revision": "f188763bcff9b91a961e",
+    "url": "/studio/static/css/main.a7730806.chunk.css"
+  },
+  {
+    "revision": "3ac1f040acd450c2f8ce",
+    "url": "/studio/static/js/2.59184578.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/studio/static/js/2.59184578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f188763bcff9b91a961e",
+    "url": "/studio/static/js/main.c695bf42.chunk.js"
+  },
+  {
+    "revision": "724e0614847bdce36a51",
+    "url": "/studio/static/js/runtime-main.4dea4635.js"
+  },
+  {
+    "revision": "a01765c06305b006416496039f82ba7f",
+    "url": "/studio/static/media/icon_mature.a01765c0.png"
+  },
+  {
+    "revision": "bb79c11df9d9093f3b4a8bc4024949f7",
+    "url": "/studio/static/media/success.bb79c11d.png"
+  }
+]);
