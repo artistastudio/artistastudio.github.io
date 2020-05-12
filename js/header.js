c05c2e3430e7be2ea956e12797b07e9d976854bd
Inna <artistastudioxyz@gmail.com>
2020-05-12 14:01:57 +1000

    Artista Colors: added search function and sidebar menu, updated layout
    (1) added search bar and js functions
    (2) added sidebar menu (needs to be adjusted for mobile)
    (3) updated layout to center the table and render correctly for different screen sizes
    (4) slightly updated the header (look and js function for handling)
    (5) updated internal links to absolute links using "/test/" etc
    (6) update logo

--- js/header.js
+++ js/header.js
@@ -1,5 +1,31 @@
+// include html
 w3IncludeHTML();
 
+function w3IncludeHTML(cb) {
+  var z, i, elmnt, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          elmnt.innerHTML = this.responseText;
+          elmnt.removeAttribute("w3-include-html");
+          w3IncludeHTML(cb);
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+  if (cb) cb();
+}
+
+
+// transfrom brger function
 function transformHamburger(e, x) {
   e.preventDefault();
   x.classList.toggle("change");
@@ -10,3 +36,30 @@ function transformHamburger(e, x) {
     menu.style.visibility = "visible";
   }
 }
+
+// animated sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
+// expand menu
+function expandMenuOnClick() { 
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
+  
+  for (i = 0; i < dropdown.length; i++) {
+
+      dropdown[i].classList.toggle("active");
+      var dropdownContent = dropdown[i].nextElementSibling;
+      
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }        
+  }
+}
+
